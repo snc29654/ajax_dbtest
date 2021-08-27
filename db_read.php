dd9d345c431d8942bf438e63aac47eefca50160d
snc29654 <snc29654@nifty.com>
2021-08-27 09:30:11 +0900

    無操作色

--- db_read.php
+++ db_read.php
@@ -76,7 +76,7 @@ file_put_contents("../id_mail.txt", "\n");
     echo "<textarea name=\"contents\" rows=\"10\" cols=\"80\" style=\"background-color:#bde9ba\" id=\"contents\" placeholder=\"内容\" >$contents</textarea>";
     echo "<textarea name=\"answer\" rows=\"10\" cols=\"80\" style=\"background-color:#bde9ba\" id=\"answer\" placeholder=\"回答\" >$answer</textarea>";
     echo "<img src=\"../jpg/$id.jpg\" width=\"150\" height=\"135\"/><br>";
-    echo "<input type= \"view\" name= \"del\"  size=\"5\"  value= \"$id\">";
+    echo "<input type= \"view\" name= \"del\"  size=\"5\"  style=\"background-color:#bde9ba\"  value= \"$id\">";
     echo "<input type= \"submit\" value= \"無操作\">";
     echo "</tbody></tr></td>";
 
