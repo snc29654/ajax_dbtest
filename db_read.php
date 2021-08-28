b92b7148e1de9615fd64c8ac68ce8ec2a5741f51
snc29654 <snc29654@nifty.com>
2021-08-28 12:04:21 +0900

    個別削除と個別回答は20レコードに限定

--- db_read.php
+++ db_read.php
@@ -77,9 +77,10 @@ file_put_contents("../id_mail.txt", "\n");
     echo "<textarea id= \"answerseg$index\" name=\"answerseg$index\" rows=\"10\" cols=\"80\" style=\"background-color:#bde9ba\" id=\"answer\" placeholder=\"回答\" >$answer</textarea>";
     echo "<img src=\"../jpg/$id.jpg\" width=\"150\" height=\"135\"/><br>";
     echo "<input type= \"text\" id= \"delseg$index\" name= \"delseg$index\"  size=\"5\"  style=\"background-color:#bde9ba\"  value= \"$id\">";
-    echo "<input  type= \"submit\" value= \"削除\" onclick=\"clickseg$index()\">";
-    echo "<input  type= \"submit\" value= \"回答\" onclick=\"clickanswerseg$index()\">";
-
+    if($index < 20){    
+        echo "<input  type= \"submit\" value= \"削除\" onclick=\"clickseg$index()\">";
+        echo "<input  type= \"submit\" value= \"回答\" onclick=\"clickanswerseg$index()\">";
+    }
     echo "<form action=\"\" method=\"post\" enctype=\"multipart/form-data\">";
     echo "<p>file：<input type=\"file\" name=\"userfile\" size=\"40\" /></p>";
     echo "<p><input type=\"text\" size=5 id=\"jpgid_value\" name=\"jpgid_value\" value=\"$id\">";
